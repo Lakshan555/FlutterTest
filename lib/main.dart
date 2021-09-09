0ec8fdbebdbe3d034206725f0a4a5e8b5691c864
Lakshan <onlineipl18@gmail.com>
2021-09-09 22:09:12 +0530

    App bar,body,FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home:Scaffold(
+void main() => runApp(MaterialApp(home: Home()));
 
-  )
-));
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first flutter App'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
 
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text('Hello'),
+      ),
 
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.redAccent,
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
